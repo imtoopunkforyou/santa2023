50c7a0a8e31fe6e0024340bc8bedf1143d4765f1
Timur Valiev <cptchunk@yandex.ru>
2023-11-16 14:53:57 +0300

    env

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # santa2023
+
+1. Указать игроков и их пожелания в players.csv
+2. Указать админов и токен бота в .env
+3. docker build . --tag santa && docker run --env-file .env santa
--- src/conf.py
+++ src/conf.py
@@ -1,10 +1,22 @@
-BOT_TOKEN = ''
-PLAYERS_WITH_WISHES = (
-    ('Pavel Durov', 'Wants to take a beautiful photo'),
-    ('Mark Zuckerberg', 'Wants to make the world virtual'),
-    ('Timur Valiev', None),
-    ('Linus Torvalds', 'He asks not to distract him from work'),
-)
-BOT_ADMINS = (
-)
-DATABASE_NAME = 'santa'
+import csv
+import os
+from typing import Tuple
+
+
+def read_players_from_csv() -> Tuple[Tuple[str, str]]:
+    """Read players from csv file at the start of the game."""
+    with open('players.csv', newline='') as csvfile:
+        spamreader = csv.reader(csvfile, delimiter=',')
+        temp = []
+        for row in spamreader:
+            if row[1] == 'None':
+                row[1] = None
+            temp.append(row)
+
+    return tuple((name, wish) for name, wish in temp[1:])
+
+
+BOT_TOKEN = os.getenv('BOT_TOKEN')
+PLAYERS_WITH_WISHES = read_players_from_csv()
+BOT_ADMINS = tuple(os.getenv('BOT_ADMINS').split(','))
+DATABASE_NAME = os.getenv('DATABASE_NAME')
